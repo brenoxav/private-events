790b87882601568c2ad79848553766fc78bee8ff
Breno Xavier <brenoxav@gmail.com>
2021-02-02 11:01:25 -0400

    Create 'Event' model, add 'creator_id' column to 'Events', style views

---
+++ db/migrate/20210201222914_add_creator_foreign_key_to_events.rb
@@ -0,0 +1,5 @@
+class AddCreatorForeignKeyToEvents < ActiveRecord::Migration[6.1]
+  def change
+    add_column :events, :creator_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_01_150639) do
+ActiveRecord::Schema.define(version: 2021_02_01_222914) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "description"
+    t.datetime "date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "creator_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
